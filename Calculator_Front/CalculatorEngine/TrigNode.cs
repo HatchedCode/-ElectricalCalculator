b8e6e68715eeb4a84645b99226113e4d7e184851
Osman <odbakari@gmail.com>
2019-09-06 12:40:39 -0700

    Add the trigNode class into the expressionTree Node

---
+++ Calculator_Front/CalculatorEngine/TrigNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    internal abstract class TrigNode : ExpressionNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstantNode"/> class.
+        /// Sets the constant value
+        /// </summary>
+        /// <param name="value">The constant value</param>
+        public TrigNode(double value)
+        {
+            this.Value = value;
+        }
+
+
+        public enum AngularMeasurement
+        {
+            Deg, Rad
+        }
+
+        /// <summary>
+        /// Gets or sets the value
+        /// </summary>
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Expression
+        /// </summary>
+        public string Expression { get; set; }
+    }
+}
