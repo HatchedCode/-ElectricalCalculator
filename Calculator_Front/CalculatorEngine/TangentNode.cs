613790849de4f7a4a4d8054d90d25c102803c9b7
Osman Bakari <odbakari@gmail.com>
2019-09-08 20:32:50 -0700

    Added the tangent class

---
+++ Calculator_Front/CalculatorEngine/TangentNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    class TangentNode : TrigNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstantNode"/> class.
+        /// Sets the constant value
+        /// </summary>
+        /// <param name="value">The constant value</param>
+        public TangentNode(double value) : base(value)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets and sets the mesurement for the trig function
+        /// </summary>
+        public AngularMeasurement angularMeasurement
+        {
+            get { return angularMeasurement; }
+            set
+            {
+                this.angularMeasurement = value;
+            }
+        }
+
+        /// <summary>
+        /// Evaluates using the built in Sine function.
+        /// </summary>
+        /// <returns>Returns the evaluation</returns>
+        public override double Evaluate()
+        {
+            return this.angularMeasurement == AngularMeasurement.Rad ? Math.Tan(this.Value) : Math. Tan(ConvertToDegree());
+        }
+    }
+}
