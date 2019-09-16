704062e33fff8d1f11f93e51b4731b590e0e766c
Marco Arceo <marcoarceo@outlook.com>
2019-09-16 10:08:32 -0700

    Added functionality for the Modulo operator

---
+++ Calculator_Front/CalculatorEngine/ModNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// <summary>
+    /// Used for modulo
+    /// </summary>
+    internal class ModNode : OperatorNode
+    {
+        /// <summary>
+        /// Gets the associativity
+        /// </summary>
+        public static Associative Associativity => Associative.Left;
+
+        /// <summary>
+        /// Gets the precedence
+        /// </summary>
+        public static ushort Precedence => 11;
+
+        /// <summary>
+        /// Gets the operator
+        /// </summary>
+        public static char Operator => '%';
+
+        /// <summary>
+        /// Evaluates the left and right
+        /// </summary>
+        /// <returns>The evaluation</returns>
+        public override double Evaluate()
+        {
+            return this.Right.Evaluate() % this.Left.Evaluate();
+        }
+    }
+}
+
