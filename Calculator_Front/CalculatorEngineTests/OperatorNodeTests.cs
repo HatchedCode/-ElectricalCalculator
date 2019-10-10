5c2a01281c24eacf34f7c3c1afd1207dd4b64a8d
Marco Arceo <marcoarceo@outlook.com>
2019-10-10 12:27:11 -0700

    Added references and allowed the internals in the engine to be visible to the tests

--- Calculator_Front/CalculatorEngineTests/OperatorNodeTests.cs
+++ Calculator_Front/CalculatorEngineTests/OperatorNodeTests.cs
@@ -1,14 +1,45 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CalculatorEngineTests
+namespace CptS321
 {
     [TestClass]
     public class OperatorNodeTests
     {
+
+        /// <summary>
+        /// This is used to test the general functionality of the addition operator
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void AdditionTesting()
         {
+
+        }
+
+        /// <summary>
+        /// This is used to test the general functionality of the subtraction operator
+        /// </summary>
+        [TestMethod]
+        public void SubtractTesting()
+        {
+
+        }
+
+        /// <summary>
+        /// This is used to test the general functionality of the multiply operator
+        /// </summary>
+        [TestMethod]
+        public void MultiplyTesting()
+        {
+
+        }
+
+        /// <summary>
+        /// This is used to test the general functionality of the division operator
+        /// </summary>
+        [TestMethod]
+        public void DivideTesting()
+        {
+
         }
     }
 }
