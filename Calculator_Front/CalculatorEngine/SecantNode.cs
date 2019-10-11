cd908710491d06d7d560508e2d2ed8aa2918f63a
Osman <odbakari@gmail.com>
2019-10-11 15:47:52 -0700

    latests with Moq

--- Calculator_Front/CalculatorEngine/SecantNode.cs
+++ Calculator_Front/CalculatorEngine/SecantNode.cs
@@ -28,7 +28,7 @@ namespace CalculatorEngine
         /// <returns>Returns the evaluation</returns>
         public override double Evaluate()
         {
-            return this.AngleMeasurement == 'R' ? (1 / Math.Cos(this.Value)) : Math.Round((1 / Math.Cos(ConvertToDegree())), 4);
+            return this.AngleMeasurement == 'R' || this.AngleMeasurement == 'r' ? (1 / Math.Cos(this.Value)) : Math.Round((1 / Math.Cos(ConvertToDegree())), 4);
         }
     }
 }
--- Calculator_Front/CalculatorEngineTests/TrigNodeTests/UnitTest1.cs
+++ Calculator_Front/CalculatorEngineTests/TrigNodeTests/UnitTest1.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CalculatorEngine;
+using NUnit.Framework;
+using Moq;
 
 namespace CalculatorEngineTests.TrigNodeTests
 {
     [TestClass]
     public class SineNodeTest
     {
+        SineNode sine;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.sine = new SineNode(90, 'd');
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestEvaluate()
         {
+            // Setup
+            Mock<SineNode> mSineNode = new Mock<SineNode>();
+
+
+            // Test to check that the sine of 90degrees is 0
+            mSineNode.Object.Evaluate();
+            
+
+
+            // Test to check that the sine of 90rad is 0.893996664
+
+            // Test to check that the sine of 0 degrees is 0
+
+            // Test to check that the sine of pi/2 is 1
+
+            // Test to check that the sine of -pi/2 is 1
+
+            //Verify that the number of calls to evaluate is 5
+            mSineNode.Verify(mock => mock.Evaluate(), Times.Exactly(5));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            this.sine = null;
         }
     }
 }
